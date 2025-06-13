2cbc849968400328529910f43fa63dc52b56e97d
Hamdi <hamdihraka@gmail.com>
2025-06-13 15:41:51 +0100

    Add experience points to player class , update related files for improved gameplay mechanics and add database

--- player-game.py
+++ player-game.py
@@ -1,19 +1,24 @@
 import json
 from random import randint
 class player:
-    def __init__(self,name,level,inventory,health):
+    def __init__(self,name,level,xp,inventory,health):
         self.name=name
         self.level=level
+        self.xp=xp
         self.inventory=inventory
         self.health=health
     def add_item(self,item):
         self.inventory.append(item)
+    def xp_bonus(self):
+        self.
     def level_up(self):
         self.level += 1
     def health_bonus(self):
         self.health += int(100/self.level)
     def show_inventory(self):
         print(self.inventory)
+with open("database.json","r")as b:
+    data=json.load(b)
 with open("player.json","r") as f:
     data=json.load(f)
 with open("command_list.json", "r") as c:
@@ -21,8 +26,8 @@ with open("command_list.json", "r") as c:
 with open("events.json","r") as d:
     ev_list=json.load(d)
 H_name=input("what is the name of your hero: ")
-hero=player(H_name,data["level"],data["inventory"], data["health"])
-while True:
+hero=player(H_name,data["level"],data["xp"],data["inventory"], data["health"])
+while hero.level != 60:
     event=str(randint)
     print(ev_list[event][1])
     command=input("what do you want to do(write //help// if you need assistance): ")
@@ -34,7 +39,7 @@ while True:
         for key , value in cmd_list.items():
          print(f"{key}:{value}")
         command=input("what do you want to do(write //help// if you need assistance): ")
+    if command=="inventory":
+        hero.show_inventory()
     
-    
-        
-                
+          
