5ee71b7f2907340df02fa976c98a220c79fcd391
Hamdi <hamdihraka@gmail.com>
2025-06-13 11:32:13 +0100

    now the hero name is customised and the health bonus is optimised

--- player-game.py
+++ player-game.py
@@ -9,13 +9,14 @@ class player:
         self.inventory.append(item)
     def level_up(self):
         self.level += 1
-    def health_bonus(self,bonus):
-        self.health += bonus
+    def health_bonus(self):
+        self.health += int(100/self.level)
 with open("player.json","r") as f:
     data=json.load(f)
-hero=player(data["name"],data["level"],data["inventory"], data["health"])
+H_name=input("what is the name of your hero")
+hero=player(H_name,data["level"],data["inventory"], data["health"])
 hero.add_item("shield")
 hero.level_up()
-hero.health_bonus(100/hero.level)
+hero.health_bonus()
 with open("player.json","w") as f:
     json.dump(hero.__dict__,f,indent=4)
